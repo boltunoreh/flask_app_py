6903db468da810dada04ddf049311e3095faad04
killer_queen <ilya.ivanov@grandrus.com>
2020-04-21 22:34:41 +0300

    catalog update

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def mainAction():
-    return '<a href="/catalog">catalog</a>'
+    return render_template('mainPage.html')
 
 
-@app.route('/catalog')
+@app.route('/catalog/')
 def catalogAction():
-    return convertProductsListToDictionary(getProducts())
+    productsDic = convertProductsListToDictionary(getProducts())
+    return render_template('catalog_ilya.html', products=productsDic)
 
 
 @app.route('/catalog/<name>')
 def catalogOneAction(name):
     productsDic = convertProductsListToDictionary(getProducts())
-
-    return productsDic[name]
+    return render_template('product.html', product=productsDic[name])
 
 
 def getProducts():
